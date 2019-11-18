33a78ae1796fcdb868ffb816b9a99b24ffc52540
1Silverfang1 <57741094+1Silverfang1@users.noreply.github.com>
2019-11-18 12:31:02 +0530

    Add files via upload

---
+++ intermediate/main.java
@@ -0,0 +1,10 @@
+class test
+{
+public static void main(String[] arg)
+{
+    String a="abc";
+    String b="bcd";
+    System.out.println(a.compareTo(b));
+    System.out.println(b.compareTo(a));
+}
+}
